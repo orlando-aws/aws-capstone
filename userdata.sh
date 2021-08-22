8f30aadb14a3933721c9e30f56ee8a6c2a7f1198
orlando-aws <ebalantekin44@gmail.com>
2021-08-22 03:59:39 +0300

    capstone

--- userdata.sh
+++ userdata.sh
@@ -3,13 +3,13 @@ apt-get update -y
 apt-get install git -y
 apt-get install python3 -y
 cd /home/ubuntu/
-TOKEN=XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
-git clone https://$TOKEN@<YOUR GITHUB REPO URL>
-cd /home/ubuntu/<YOUR GITHUB REPO NAME>
+TOKEN="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"
+git clone https://$TOKEN@github.com/orlando-aws/aws-capstone
+cd /home/ubuntu/aws-capstone
 apt install python3-pip -y
 apt-get install python3.7-dev libmysqlclient-dev -y
 pip3 install -r requirements.txt
-cd /home/ubuntu/<YOUR GITHUB REPO NAME>/src
+cd /home/ubuntu/aws-capstone/src
 python3 manage.py collectstatic --noinput
 python3 manage.py makemigrations
 python3 manage.py migrate
